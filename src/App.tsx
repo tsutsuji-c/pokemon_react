c0a459e067e1f9855886ed8e6d191026e9ba7e9b
tsutsuji-c <camelli_043@icloud.com>
2021-06-01 13:53:24 +0900

    typescript対応

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme/theme"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return ( <ChakraProvider theme={theme}>
+   <div>
+     <h1>HellowWorld</h1>
+   </div>
+  </ChakraProvider>);
 }
-
-export default App;
